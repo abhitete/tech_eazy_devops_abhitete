3a5af7d06298fcac6c76b862f4da34b7f487ade6
Abhinay Tete <abhinaystete@gmail.com>
2025-09-29 16:42:47 +0530

    Delete output.tf

--- output.tf
+++
@@ -1,8 +0,0 @@
-output "instance_public_ip_javaapp" {
-  description = "Public IP of the JavaApp EC2 instance"
-  value       = aws_instance.JavaApp_EC2.public_ip
-}
-output "s3_bucket_name" {
-  description = "Name of the S3 bucket for logs"
-  value       = aws_s3_bucket.logs_bucket.id
-}
